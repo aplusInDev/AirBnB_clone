34a4b8b38f530dc2ac041145c295c222ea542c8c
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-08-13 12:59:29 +0100

    update User file base_model and more classes

--- models/review.py
+++ models/review.py
@@ -8,9 +8,6 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """A class that inherits from BaseModel and represents a review"""
 
-    # A public class attribute that stores the place ID of the review
     place_id: str = ""
-    # A public class attribute that stores the user ID of the review
     user_id: str = ""
-    # A public class attribute that stores the text of the review
     text: str = ""
