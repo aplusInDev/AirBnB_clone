cea63e782b1785cbdf1a975a95373f807ebebc36
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-03 12:57:49 +0000

    fixing tests dir

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -21,12 +21,6 @@ class test_basemodel(unittest.TestCase):
         """ """
         pass
 
-    def tearDown(self):
-        try:
-            os.remove('file.json')
-        except:
-            pass
-
     def test_default(self):
         """ """
         i = self.value()
