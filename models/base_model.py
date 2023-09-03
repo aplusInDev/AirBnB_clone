8ab9ba36ef0d9ac357ce9dd4c51567e5b92f1c31
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-03 13:16:37 +0100

    update baseModel to fixe to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -63,7 +63,8 @@ class BaseModel():
             including a key called __class__ with the class
             name as its value, and updated_at and created_at
             attributes converted from datetime objects to strings."""
-        my_dict = self.__dict__.copy()
+        my_dict = {}
+        my_dict.update(self.__dict__)
         my_dict['__class__'] = type(self).__name__
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['created_at'] = self.created_at.isoformat()
