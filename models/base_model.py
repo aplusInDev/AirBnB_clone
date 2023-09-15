fea5e13954fe270b7af6b041bbee9155502c8085
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-15 15:59:32 +0100

    revert changes from to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -63,10 +63,8 @@ class BaseModel():
             including a key called __class__ with the class
             name as its value, and updated_at and created_at
             attributes converted from datetime objects to strings."""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = type(self).__name__
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        return my_dict
