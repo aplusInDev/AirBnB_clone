3f15c771414dea944e6cae76d37ab833c93a84f4
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-17 21:32:33 +0000

    fixing pycodestyle

--- tests/test_console.py
+++ tests/test_console.py
@@ -18,14 +18,14 @@ class HBNBCommandTest(unittest.TestCase):
     def setUp(self):
         # create some mock objects for testing
         self.user = User(name="Alice", email="alice@example.com")
-        self.place = Place(
-            name="Hotel", address="123 Main Street", city="New York", state="NY")
+        self.place = Place(name="Hotel", address="123 Main Street", city="New \
+        York", state="NY")
         self.state = State(name="New York", country="USA")
         self.city = City(name="New York", state=self.state)
         self.amenity = Amenity(
             name="Free wifi", description="Free wifi in the hotel")
-        self.review = Review(text="Great hotel, friendly staff, good location",
-                             rating=5, user=self.user, place=self.place)
+        self.review = Review(text="Great hotel, friendly staff, good\
+                location", rating=5, user=self.user, place=self.place)
         # create a mock storage object for testing
         self.storage = storage.Storage()
         # create a mock sys object for testing
@@ -36,13 +36,10 @@ class HBNBCommandTest(unittest.TestCase):
             # patch the sys.path list with an empty list
             with unittest.mock.patch("sys.path") as p:
                 p.return_value = []
-            # create an instance of HBNBCommand class with the mocked sys objects
             self.cmd = HBNBCommand(sys=sys, storage=self.storage)
 
     def test_do_help(self):
-        # call the do_help method of the cmd object with an argument "help show"
         result = self.cmd.do_help("help show")
-        # assert that the result contains the expected message "Usage: help argument"
         self.assertEqual(result, "Usage: help argument")
 
     def test_do_show(self):
@@ -52,7 +49,6 @@ class HBNBCommandTest(unittest.TestCase):
         self.assertEqual(result, "Alice")
 
     def test_do_show(self):
-        # call the do_show method of the cmd object with an argument "123 Main Street"
         result = self.cmd.do_show("123 Main Street")
         # assert that the result contains the expected message "Hotel"
         self.assertEqual(result, "Hotel")
@@ -64,15 +60,12 @@ class HBNBCommandTest(unittest.TestCase):
         self.assertEqual(result, "New York")
 
     def test_do_show(self):
-        # call the do_show method of the cmd object with an argument "New York NY"
         result = self.cmd.do_show("New York NY")
         # assert that the result contains the expected message "Hotel"
         self.assertEqual(result, "Hotel")
 
     def test_do_show(self):
-        # call the do_show method of the cmd object with an argument "New York NY USA"
         result = self.cmd.do_show("New York NY USA")
-        # assert that the result contains all three messages: Hotel, New York, and USA
         self.assertIn("Hotel", result)
         self.assertIn("New York", result)
         self.assertIn("USA", result)
@@ -84,13 +77,11 @@ class HBNBCommandTest(unittest.TestCase):
         pass
 
     def test_do_destroy(self):
-        # call the do_destroy method of the cmd object with an argument "123 Main Street"
         result = self.cmd.do_destroy("123 Main Street")
         # assert that no exception is raised and no output is printed
         pass
 
     def test_do_destroy(self):
-        # call the do_destroy method of the cmd object with an argument "New York NY USA"
         result = self.cmd.do_destroy("New York NY USA")
         # assert that no exception is raised and no output is printed
         pass
