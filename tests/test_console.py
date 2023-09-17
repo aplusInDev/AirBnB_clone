b66c7f0520f7ff45b8b669a9c3c0b005dd168b5a
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-17 22:07:40 +0100

    uploading test_console

---
+++ tests/test_console.py
@@ -0,0 +1,96 @@
+import unittest
+import io
+import sys
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.__init__ import storage
+from console import HBNBCommand
+
+
+class HBNBCommandTest(unittest.TestCase):
+
+    def setUp(self):
+        # create some mock objects for testing
+        self.user = User(name="Alice", email="alice@example.com")
+        self.place = Place(
+            name="Hotel", address="123 Main Street", city="New York", state="NY")
+        self.state = State(name="New York", country="USA")
+        self.city = City(name="New York", state=self.state)
+        self.amenity = Amenity(
+            name="Free wifi", description="Free wifi in the hotel")
+        self.review = Review(text="Great hotel, friendly staff, good location",
+                             rating=5, user=self.user, place=self.place)
+        # create a mock storage object for testing
+        self.storage = storage.Storage()
+        # create a mock sys object for testing
+        self.sys = unittest.mock.Mock()
+        # patch the sys.stdout object with a StringIO object
+        with unittest.mock.patch("sys.stdout") as f:
+            f.return_value = unittest.mock.Mock(spec=io.StringIO())
+            # patch the sys.path list with an empty list
+            with unittest.mock.patch("sys.path") as p:
+                p.return_value = []
+            # create an instance of HBNBCommand class with the mocked sys objects
+            self.cmd = HBNBCommand(sys=sys, storage=self.storage)
+
+    def test_do_help(self):
+        # call the do_help method of the cmd object with an argument "help show"
+        result = self.cmd.do_help("help show")
+        # assert that the result contains the expected message "Usage: help argument"
+        self.assertEqual(result, "Usage: help argument")
+
+    def test_do_show(self):
+        # call the do_show method of the cmd object with an argument "Alice"
+        result = self.cmd.do_show("Alice")
+        # assert that the result contains the expected message "Alice"
+        self.assertEqual(result, "Alice")
+
+    def test_do_show(self):
+        # call the do_show method of the cmd object with an argument "123 Main Street"
+        result = self.cmd.do_show("123 Main Street")
+        # assert that the result contains the expected message "Hotel"
+        self.assertEqual(result, "Hotel")
+
+    def test_do_show(self):
+        # call the do_show method of the cmd object with an argument "New York"
+        result = self.cmd.do_show("New York")
+        # assert that the result contains the expected message "New York"
+        self.assertEqual(result, "New York")
+
+    def test_do_show(self):
+        # call the do_show method of the cmd object with an argument "New York NY"
+        result = self.cmd.do_show("New York NY")
+        # assert that the result contains the expected message "Hotel"
+        self.assertEqual(result, "Hotel")
+
+    def test_do_show(self):
+        # call the do_show method of the cmd object with an argument "New York NY USA"
+        result = self.cmd.do_show("New York NY USA")
+        # assert that the result contains all three messages: Hotel, New York, and USA
+        self.assertIn("Hotel", result)
+        self.assertIn("New York", result)
+        self.assertIn("USA", result)
+
+    def test_do_destroy(self):
+        # call the do_destroy method of the cmd object with an argument "Alice"
+        result = self.cmd.do_destroy("Alice")
+        # assert that no exception is raised and no output is printed
+        pass
+
+    def test_do_destroy(self):
+        # call the do_destroy method of the cmd object with an argument "123 Main Street"
+        result = self.cmd.do_destroy("123 Main Street")
+        # assert that no exception is raised and no output is printed
+        pass
+
+    def test_do_destroy(self):
+        # call the do_destroy method of the cmd object with an argument "New York NY USA"
+        result = self.cmd.do_destroy("New York NY USA")
+        # assert that no exception is raised and no output is printed
+        pass
