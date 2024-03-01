e53cfa2bc1b250403f5e79abfa190408c709004e
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-03-01 22:07:07 +0100

    test all method in console

--- console.py
+++ console.py
@@ -108,18 +108,35 @@ class HBNBCommand(cmd.Cmd):
                 except ImportError:
                     print("** class doesn't exist **")
 
-    def do_all(self, arg):
-        """ Prints all string representation of all instances based
-        """
+    # def do_all(self, arg):
+    #     """ Prints all string representation of all instances based
+    #     """
 
-        if arg and arg in HBNBCommand.classes_list:
-            class_name = eval(arg)
+    #     if arg and arg in HBNBCommand.classes_list:
+    #         class_name = eval(arg)
+    #         for key, value in storage.all().items():
+    #             if value.__class__ == class_name:
+    #                 print(value)
+    #     elif arg:
+    #         print("** class doesn't exist **")
+    #         return None
+
+    def do_all(self, line):
+        '''all command that show all giving class instances
+        Usage: all <class name>'''
+        all_list = []
+        if line:
+            if line not in HBNBCommand.class_dict:
+                print("** class doesn't exist **")
+                return
             for key, value in storage.all().items():
-                if value.__class__ == class_name:
-                    print(value)
-        elif arg:
-            print("** class doesn't exist **")
-            return None
+                if line == key.split(".")[0]:
+                    all_list.append(str(value))
+        else:
+            for key, value in storage.all().items():
+                all_list.append(str(value))
+                pass
+        print(all_list)
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id by adding or
